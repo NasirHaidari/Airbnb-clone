e13e8f411715ba362f7f8550a1f2689235d29a59
NasirHaidari <nasir.haidri@htomail.com>
2021-08-09 17:24:23 +0200

    the map and search is working

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ['links.papareact.com'],
   },
+  env: {
+    mapbox_key:
+      'pk.eyJ1IjoibmFzaXItZGV2IiwiYSI6ImNrczN2bWVpMzAxMW4yb3MzNjY2bDR2MTUifQ.EK5aZHqylI0I-ms6_E3l4Q',
+  },
 }
